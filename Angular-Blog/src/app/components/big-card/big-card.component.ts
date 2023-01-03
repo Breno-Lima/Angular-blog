f5bc20d5a3259406d8b4c40d154fb995669f96cb
Breno-Lima <brenohslima@hotmail.com>
2023-01-02 23:17:44 -0300

    blog-angular

---
+++ Angular-Blog/src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,22 @@
+import { Component ,Input} from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.css']
+})
+export class BigCardComponent {
+  @Input()
+   id ="0"
+
+  @Input() 
+  cardFoto =""
+  @Input()
+  cardTitle ="Titulo do Artigo"
+  @Input()
+  cardDescription =""
+
+  constructor(){
+
+  }
+}
